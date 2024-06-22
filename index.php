7d8a91a2665b4b792507a4e702e1d635df8fe842
jit-ghosh <73941250+jit-ghosh@users.noreply.github.com>
2024-06-22 21:48:52 +0530

    initial push

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connected succesfully";
+}
+
+// $sql = "SELECT id, firstname, lastname FROM MyGuests";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//     }
+// } else {
+//     echo "0 results";
+// }
+// $conn->close();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+        $sql = "SELECT * FROM entityclass";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["slno"]. " - Name: " . $row["entity_name"]. " " . $row["fatca"]." " . $row["crs"]" " . $row["rationale"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+    ?>
+</body>
+</html>
