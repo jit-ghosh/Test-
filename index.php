3be8b17c2e3694789bec2461e9919372016ae44c
Soumyadeep <dey.soumyadeep123@gmail.com>
2024-07-11 12:34:54 +0530

    Demo Working

--- index.php
+++ index.php
@@ -12,49 +12,70 @@ include 'conn.php'
     <title>Zeo Data_Demo sheet</title>
 </head>
 <body >
-    <div class="d-flex justify-content-center align-items-center p-10 m-10 ">
+
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Website</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <table class='table table-striped'>
+            <thead>
+                <tr>
+                    <th>slno</th>
+                    <th>Entity Name</th>
+                    <th>country</th>
+                    <th>Fatca</th>
+                    <th>crs</th>
+                    <th>rationale</th>
+                </tr>
+            </thead>
 
     <?php
         $data_fetcher = "select * from entityclass";
         if ($result = $conn -> query($data_fetcher)) {
             $c=0;
             while ($row = $result -> fetch_row()) {
-           
-                echo "<table class='table'>";
-                if($c=0){ 
-                    echo "
-                    <thead>
-                        <tr>
-                            <th scope='col'>".$row[0]."</th>
-                            <td scope='col'>".$row[1]."</td>
-                            <td scope='col'>".$row[2]."</td>
-                            <td scope='col'>".$row[3]."</td>
-                            <td scope='col'>".$row[4]."</td>
-                            <td scope='col'>".$row[5]."</td>
-                        </tr>
-                    </thead>";
 
-                    $c+=1;
-                }else {
-                    
-                    echo "<tbody>
-                      <tr>
-                      <th scope='col'>".$row[0]."</th>
-                      <td scope='col'>".$row[1]."</td>
-                      <td scope='col'>".$row[2]."</td>
-                      <td scope='col'>".$row[3]."</td>
-                      <td scope='col'>".$row[4]."</td>
-                      <td scope='col'>".$row[5]."</td>
-                      </tr>
-                      </tbody>";
+                    echo "
+                    <tbody>
+                        <tr>
+                            <td>".$row[0]."</td>
+                            <td>".$row[1]."</td>
+                            <td>".$row[2]."</td>
+                            <td>".$row[3]."</td>
+                            <td>".$row[4]."</td>
+                            <td>".$row[5]."</td>
+                        </tr>                    
+                    </tbody>";
                 }
                 echo "</table>";     
             }
-        }
+        
           
     ?>
-    
-        
+      
     </div>
     
 
